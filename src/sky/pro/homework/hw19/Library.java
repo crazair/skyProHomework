db5fbbff3e08c557e55bf0d6fe914315cca47efd
crazair <crazzyzyzy@rambler.ru>
2022-08-26 10:30:11 +0300

    Домашка 1.9 Объекты и классы

---
+++ src/sky/pro/homework/hw19/Library.java
@@ -0,0 +1,39 @@
+package sky.pro.homework.hw19;
+
+import java.util.Arrays;
+
+public class Library {
+
+    private final Book[] books;
+
+    public Library(int lenMass) {
+        books = new Book[lenMass];
+    }
+
+    public Library(Book[] books) {
+        this.books = books;
+    }
+
+    private Book[] addBook(Book[] books, Book book) {
+        Book[] newBooks = new Book[books.length + 1];
+        System.arraycopy(books, 0, newBooks, 0, books.length);
+        newBooks[books.length] = book;
+        return newBooks;
+    }
+
+    public void printBooks() {
+        Arrays.stream(books).forEach(System.out::println);
+    }
+
+    public void printBookByName(String bookName) {
+        Arrays.stream(books)
+                .filter(book -> book.getName().equals(bookName))
+                .forEach(book -> System.out.println(book.stringBookToPrint()));
+    }
+
+    public void changeYearPublicationByName(String bookName, int newYearPublication) {
+        Arrays.stream(books)
+                .filter(book -> book.getName().equals(bookName))
+                .forEach(book -> book.setYearPublication(newYearPublication));
+    }
+}
